d5c863bb81ef14b85a05678f2c9a236181ba0907
Rishabh Tayal <rtayal11@gmail.com>
2016-04-04 16:19:42 -0500

    comit

--- cloud/main.js
+++ cloud/main.js
@@ -3,24 +3,24 @@ Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
 
-// Parse.Cloud.define("notification", function(request, response) {
-//     // Parse.Cloud.useMasterKey();
-//     var query = notification.query;
-//     console.log(query);
-//     Parse.Push.send({
-//         // channels: ["channel"],
-//         where: query
-//         data: {
-//             alert: "test"
-//         }
-//     }, {
-//     	useMasterKey: true,
-//         success: function() {
-//             // status.success("Push sent to all users");
-//             response.success("push sent");
-//         },
-//         error: function(error) {
-//             response.error("Error sending pushes: " + error.code + ": " + error.message);
-//         }
-//     });
-// });
+Parse.Cloud.define("notification", function(request, response) {
+    // Parse.Cloud.useMasterKey();
+    var query = request.params.query;
+    console.log(query);
+    Parse.Push.send({
+        // channels: ["channel"],
+        where: query,
+        data: {
+            alert: "test"
+        }
+    }, {
+    	useMasterKey: true,
+        success: function() {
+            // status.success("Push sent to all users");
+            response.success("push sent");
+        },
+        error: function(error) {
+            response.error("Error sending pushes: " + error.code + ": " + error.message);
+        }
+    });
+});
